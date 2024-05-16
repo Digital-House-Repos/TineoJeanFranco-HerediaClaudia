5b5f5709c556a30a2221004a10af8cfeee3590d3
tineojf <92743762+tineodev@users.noreply.github.com>
2024-05-15 21:49:11 -0500

    Item 3 - log4j

--- src/dao/OdontologoEnMemoriaDAO.java
+++ src/dao/OdontologoEnMemoriaDAO.java
@@ -1,11 +1,13 @@
 package dao;
 
 import models.Odontologo;
+import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
 
 public class OdontologoEnMemoriaDAO implements IDAO<Odontologo> {
     private final ArrayList<Odontologo> odontologos;
+    private static final Logger logger = Logger.getLogger(OdontologoEnMemoriaDAO.class);
 
     public OdontologoEnMemoriaDAO() {
         this.odontologos = new ArrayList<>();
@@ -13,6 +15,7 @@ public class OdontologoEnMemoriaDAO implements IDAO<Odontologo> {
 
     @Override
     public void guardar(Odontologo odontologo) {
+        logger.info("Guardando odontologo en memoria");
         odontologos.add(odontologo);
     }
 
@@ -23,6 +26,7 @@ public class OdontologoEnMemoriaDAO implements IDAO<Odontologo> {
 
     @Override
     public ArrayList<Odontologo> listar() {
+        logger.info("Listando odontologos en memoria");
         return odontologos;
     }
 }
--- src/dao/OdontologoH2DAO.java
+++ src/dao/OdontologoH2DAO.java
@@ -3,6 +3,7 @@ package dao;
 import java.sql.Connection;
 
 import models.Odontologo;
+import org.apache.log4j.Logger;
 import persistence.DBConnector;
 
 import java.sql.Connection;
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 
 
 public class OdontologoH2DAO implements IDAO<Odontologo> {
+    private static final Logger logger = Logger.getLogger(OdontologoH2DAO.class);
 
     private static final DBConnector connector = DBConnector.getInstance();
     private static final Connection connection = connector.getConnection();
@@ -29,9 +31,11 @@ public class OdontologoH2DAO implements IDAO<Odontologo> {
             while (result.next()) {
                 listDB.add(new Odontologo(result.getInt("id"), result.getString("numeroMatricula"), result.getString("nombre"), result.getString("apellido")));
             }
+            logger.info("Listando odontologos en H2");
 
             return listDB;
         } catch (SQLException e) {
+            logger.error("Error al listar odontologos en H2");
             System.out.println("GET error: " + e.getMessage());
             return null;
         }
@@ -48,11 +52,14 @@ public class OdontologoH2DAO implements IDAO<Odontologo> {
 
             int rowsInserted = preparedStatement.executeUpdate();
             if (rowsInserted > 0) {
+                logger.info("Guardando odontologo en H2");
                 System.out.println("POST - CREATED");
             } else {
+                logger.error("Error al guardar odontologo en H2");
                 System.out.println("POST - NOT CREATED");
             }
         } catch (SQLException e) {
+            logger.error("Error al guardar odontologo en H2");
             System.err.println("POST error: " + e.getMessage());
         }
     }
